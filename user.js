0fe532cb6b2360c8794d4219500f621a1d66c7be
daoa93 <daoa93@naver.com>
2025-04-24 14:49:55 +0900

    feat:삭제 기능 추가가

--- user.js
+++ user.js
@@ -15,9 +15,29 @@ async function getUsers(pageNum = 0) {
         <h2>${user.uname}</h2>
         <p>${user.email}</p>
         <span>${user.colors}</span>
+        <button class='btnDel' data-id=${user.id}>delete</button>
       </article>
     `;
   }); //tags 반복 끝
 
   section.innerHTML = tags;
+
+  const btns = document.querySelectorAll('.btnDel');
+
+  btns.forEach((btn) => {
+    btn.addEventListener("click", async (e) => {
+      const memberId = e.currentTarget.getAttribute("data-id");
+      console.log(memberId);
+
+      const data = await fetch(
+        `http://127.0.0.1:8080/api/admin/del/${memberId}`,
+        {
+          method: "DELETE",
+        }
+      );
+      result = await data.json();
+      alert(result.message);
+      location.href = "index.html";
+    });
+  });
 }
